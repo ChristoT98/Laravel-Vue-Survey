ace8a9228de3aec0f4d28e39d8cc448fd687b37c
ChristoT98 <christolakshan@gmail.com>
2022-04-08 10:31:53 +0530

    Login function completed

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -7,22 +7,22 @@
           <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500"> register for free </router-link>
         </p>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6" @submit="login">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+            <input id="email-address" name="email" v-model="user.email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+            <input id="password" name="password" v-model="user.password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
           </div>
         </div>
 
         <div class="flex items-center justify-between">
           <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+            <input id="remember-me" name="remember-me" v-model="user.remeber" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
             <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
           </div>
         </div>
@@ -41,13 +41,27 @@
       </form>
 </template>
 
-<script>
+<script setup>
 import { LockClosedIcon } from '@heroicons/vue/solid'
+import store from "../store";
+import {useRouter} from "vue-router";
+const router = useRouter();
 
-export default {
-  components: {
-    LockClosedIcon,
-  },
+const user = {
+  email: '',
+  password: '',
+  remeber: false
+}
+
+function login(ev) {
+  ev.preventDefault();
+
+  store.dispatch('login', user)
+  .then(()=> {
+    router.push({
+      name: 'Dashboard'
+    })
+  })
 }
 </script>
 
