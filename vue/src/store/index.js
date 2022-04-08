b90b640a34097a50cdd878e0dd4ed17452e1ff4d
ChristoT98 <christolakshan@gmail.com>
2022-04-08 11:30:58 +0530

    Logout function created successfully

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -11,18 +11,26 @@ const store = createStore({
   getters: {},
   actions: {
     register({commit}, user) {
-        return axiosClient.post('/register', user)
-          .then(({data})=> {
-            commit('setUser', data);
-            return data;
-          })
+      return axiosClient.post('/register', user)
+        .then(({data}) => {
+          commit('setUser', data);
+          return data;
+        })
     },
 
     login({commit}, user) {
       return axiosClient.post('/login', user)
-        .then(({data})=> {
+        .then(({data}) => {
           commit('setUser', data);
           return data;
+        })
+    },
+
+    logout({commit}) {
+      return axiosClient.post('/logout')
+        .then((response) => {
+          commit('logout');
+          return response;
         })
     },
 
@@ -31,12 +39,14 @@ const store = createStore({
     logout: (state) => {
       state.user.data = {};
       state.user.token = null;
+      state.user.data = {}
+      sessionStorage.removeItem('TOKEN')
     },
     setUser: (state, userData) => {
       state.user.token = userData.token
       state.user.data = userData.user
       sessionStorage.setItem('TOKEN', userData.token)
-}
+    }
   },
   modules: {}
 })
